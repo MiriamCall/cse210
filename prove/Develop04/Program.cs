aa17e9d30a7403e50e8ca5e46bf7622792e5f169
MiriamCall <bow18005@byui.edu>
2024-07-02 16:54:26 -0600

    Refactor Develop04 Program.cs to add menu functionality
    The Develop04 Program.cs file has been refactored to include a new method, DisplayMenu(), which displays a menu of activities for the user to choose from. This improves the structure and user experience of the program.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        public void DisplayMenu()
+        {
+             while (choice !=4) {
+                Console.WriteLine("1. Start Breathing Activity");
+                Console.WriteLine("2. Start Reflection Activity");
+                Console.WriteLine("3. Start Listing Activity");
+                Console.WriteLine("4. Quit");
+
+                string input = Console.ReadLine();
+                if(int.TryParse(input, out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                        break;
+                        case 2:
+                        break;
+                        case 3:
+                        break;
+                        case 4:
+                        Environment.Exit(0);
+                        break;
+                    }
+                }
+             }
+        }
     }
 }
