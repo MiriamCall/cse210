ced6bac7f905c297256d390a63bb3405c6895e10
MiriamCall <bow18005@byui.edu>
2024-07-02 16:43:36 -0600

    Refactor code in Develop04 activities to improve structure and encapsulation

---
+++ prove/Develop04/Breathing.cs
@@ -0,0 +1,7 @@
+class Breathing : Activity
+{
+    private
+
+
+    description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+}
