8985939afdecf91ed92b0fad0adece84755dc6a3
MiriamCall <bow18005@byui.edu>
2024-07-12 11:43:57 -0600

    Refactor SimpleGoal class to improve encapsulation and add achievement tracking

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -1,11 +1,34 @@
-class SimpleGoal : Goal{
+class SimpleGoal : Goal
+{
+    public string Name{
+        get;
+        private set;
+    }
+
+    public string Description {
+        get;
+        private set;
+    }
+
+    public bool IsAchieved{
+        get;
+        private set;
+    }
+
+    public SimpleGoal(string name, string description) {
+        Name = name;
+        Description = description;
+        IsAchieved = false;
+    }
+
     public override string GetGoalType()
     {
-        return "";
+        return "SimpleGoal";
     }
     public override int RecordEvent()
     {
-        return 0;
+        IsAchieved = true;
+        return 1;
     }
     public override void RunGoal()
     {
