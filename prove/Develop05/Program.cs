9136f2b5876418f8c6b4d0104361dbe88375dc46
MiriamCall <bow18005@byui.edu>
2024-07-14 13:23:56 -0600

    feat: Add bad habit tracker to Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,6 @@
 using System;
 
+// To exceed the requirements of the project I added a bad habit tracker. It subtracts points from the total points when a bad habit is recorded. 
 class Program
 {
     static void Main(string[] args)
