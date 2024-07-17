1437b218e3d3101f684152b42078a3b6e218e642
MiriamCall <bow18005@byui.edu>
2024-07-17 15:03:46 -0600

    Refactor FeedingLog class to include start and end time properties and display feeding log

--- final/FinalProject/FeedingLog.cs
+++ final/FinalProject/FeedingLog.cs
@@ -1,4 +1,25 @@
 class FeedingLog
 {
-    
+    private DateTime startTime;
+    private DateTime endTime;
+
+    public FeedingLog()
+    {
+        startTime = DateTime.Now;
+        endTime = DateTime.Now;
+    }
+    public void DisplayFeedingLog()
+    {
+        Console.WriteLine("Feeding Log");
+        Console.WriteLine("Start Time: " + startTime);
+        Console.WriteLine("End Time: " + endTime);
+    }
+
+    // public GetStartTime()
+
+    // public SetStartTime()
+
+    // public GetEndTime()
+
+    // public SetEndTime()
 }
