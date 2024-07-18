61ea6fbd076979528b759af2fe6d3a8baf7487e2
MiriamCall <bow18005@byui.edu>
2024-07-18 14:46:14 -0600

    feat: Add logging functionality to FileHandler class

--- final/FinalProject/FileHandler.cs
+++ final/FinalProject/FileHandler.cs
@@ -1,4 +1,36 @@
 class FileHandler
 {
-    
+    private List<FeedingLog> _feedingLogs = new List<FeedingLog>();
+
+    public void AddFeedingLog(FeedingLog log)
+    {
+        _feedingLogs.Add(log);
+    }
+
+    public FeedingLog GetFeedingLog(FeedingLog log)
+    {
+        return log;
+    }
+
+    public void DisplayLogs()
+    {
+        foreach (FeedingLog log in _feedingLogs)
+        {
+            log.DisplayFeedingLog();
+        }
+    }
+
+    public void SaveLogsToFile(string fileName)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            foreach (FeedingLog log in _feedingLogs)
+            {
+                writer.WriteLine(log.ToString());
+            }
+        }
+    }
+    {
+        
+    }
 }
